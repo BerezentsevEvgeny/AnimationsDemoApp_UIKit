67693ccce162c2d39a948776d9a0fb017672c84c
Berezentsev Evgeny <jonn09@icloud.com>
2021-08-04 09:32:09 +0300

    Initial Commit

---
+++ AnimationsTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimationsTest
+//
+//  Created by Евгений Березенцев on 04.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
